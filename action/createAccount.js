cfe57a96e6ccf3d2fb97ac5e757484b3b3fbb125
MehediHasan4321 <126mehedi@gmail.com>
2024-05-07 14:25:06 +0600

    update some data

--- action/createAccount.js
+++ action/createAccount.js
@@ -3,28 +3,28 @@ import bcrypt from 'bcryptjs'
 import findUserByEmail from './findUserByEmail'
 
 
- const createAccount = async (value) => {
+const createAccount = async (value) => {
     const valideataData = registerSchema.safeParse(value)
     if (!valideataData.success) {
         return { error: 'Invalid Data!' }
     }
     const { name, email, password } = valideataData.data
 
-    const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const bodyData = {
         name,
         email,
-       password:hashedPassword,
+        password: hashedPassword,
         role: 'user',
         createAt: new Date(),
         updateAt: new Date()
     }
 
     const isAlreadyExist = await findUserByEmail(email)
-   
-    if(isAlreadyExist){
-        return {error:'Email Already taken!'}
+
+    if (isAlreadyExist) {
+        return { error: 'Email Already taken!' }
     }
 
     const res = await fetch('http://localhost:8000/users', {
@@ -37,11 +37,11 @@ import findUserByEmail from './findUserByEmail'
 
     const data = await res.json()
 
-    if(data.insertedId){
-        return {message:'User Created'}
+    if (data.insertedId) {
+        return { message: 'User Created' }
     }
 
-    return {error:'Something went wrong!'}
+    return { error: 'Something went wrong!' }
 
 }
 
